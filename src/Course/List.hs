389a70fbece461757fd7f2c1cb548cb6de5e1f1d
Jason Davidson <jad658@gmail.com>
2020-10-04 21:33:54 -0700

    Implement list functions

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -75,8 +75,7 @@ headOr ::
   a
   -> List a
   -> a
-headOr =
-  error "todo: Course.List#headOr"
+headOr x = foldRight const x
 
 -- | The product of the elements of a list.
 --
@@ -91,8 +90,7 @@ headOr =
 product ::
   List Int
   -> Int
-product =
-  error "todo: Course.List#product"
+product = foldRight (*) 1
 
 -- | Sum the elements of the list.
 --
@@ -106,8 +104,7 @@ product =
 sum ::
   List Int
   -> Int
-sum =
-  error "todo: Course.List#sum"
+sum = foldRight (+) 0
 
 -- | Return the length of the list.
 --
@@ -118,8 +115,7 @@ sum =
 length ::
   List a
   -> Int
-length =
-  error "todo: Course.List#length"
+length = foldRight (\_ n -> n+1) 0
 
 -- | Map the given function on each element of the list.
 --
@@ -133,8 +129,7 @@ map ::
   (a -> b)
   -> List a
   -> List b
-map =
-  error "todo: Course.List#map"
+map f = foldRight (\x xs -> f x :. xs) Nil
 
 -- | Return elements satisfying the given predicate.
 --
@@ -150,8 +145,7 @@ filter ::
   (a -> Bool)
   -> List a
   -> List a
-filter =
-  error "todo: Course.List#filter"
+filter f = foldRight (\x -> if f x then (x:.) else id) Nil
 
 -- | Append two lists to a new list.
 --
@@ -169,8 +163,7 @@ filter =
   List a
   -> List a
   -> List a
-(++) =
-  error "todo: Course.List#(++)"
+xs ++ ys = foldRight (:.) ys xs
 
 infixr 5 ++
 
@@ -187,8 +180,7 @@ infixr 5 ++
 flatten ::
   List (List a)
   -> List a
-flatten =
-  error "todo: Course.List#flatten"
+flatten = foldRight (++) Nil
 
 -- | Map a function then flatten to a list.
 --
@@ -204,8 +196,7 @@ flatMap ::
   (a -> List b)
   -> List a
   -> List b
-flatMap =
-  error "todo: Course.List#flatMap"
+flatMap f = flatten . map f
 
 -- | Flatten a list of lists to a list (again).
 -- HOWEVER, this time use the /flatMap/ function that you just wrote.
@@ -214,8 +205,7 @@ flatMap =
 flattenAgain ::
   List (List a)
   -> List a
-flattenAgain =
-  error "todo: Course.List#flattenAgain"
+flattenAgain = flatMap id
 
 -- | Convert a list of optional values to an optional list of values.
 --
@@ -242,8 +232,10 @@ flattenAgain =
 seqOptional ::
   List (Optional a)
   -> Optional (List a)
-seqOptional =
-  error "todo: Course.List#seqOptional"
+seqOptional = foldRight f (Full Nil)
+  where
+    f (Full x) (Full xs) = Full (x:.xs)
+    f _ _ = Empty
 
 -- | Find the first element in the list matching the predicate.
 --
@@ -265,8 +257,11 @@ find ::
   (a -> Bool)
   -> List a
   -> Optional a
-find =
-  error "todo: Course.List#find"
+find p = foldRight f Empty
+  where
+    f x accum
+      | p x = Full x
+      | otherwise = accum
 
 -- | Determine if the length of the given list is greater than 4.
 --
@@ -284,8 +279,8 @@ find =
 lengthGT4 ::
   List a
   -> Bool
-lengthGT4 =
-  error "todo: Course.List#lengthGT4"
+lengthGT4 (_:._:._:._:._:._)= True
+lengthGT4 _ = False
 
 -- | Reverse a list.
 --
@@ -301,8 +296,7 @@ lengthGT4 =
 reverse ::
   List a
   -> List a
-reverse =
-  error "todo: Course.List#reverse"
+reverse = foldLeft (flip (:.)) Nil
 
 -- | Produce an infinite `List` that seeds with the given value at its head,
 -- then runs the given function for subsequent elements
@@ -331,7 +325,7 @@ notReverse ::
   List a
   -> List a
 notReverse =
-  error "todo: Is it even possible?"
+  error "not possible"
 
 ---- End of list exercises
 
--- src/Test/Framework/Property.hs
+++ src/Test/Framework/Property.hs
@@ -52,11 +52,12 @@ sized :: Integral i => (i -> Gen a) -> Gen a
 sized f = Gen $ \size gen -> runGen (f $ fromIntegral size) size gen
 
 frequency :: [(Int, Gen a)] -> Gen a
-frequency xs = 
+frequency xs =
   let tot = sum (fst <$> xs)
-      pick n ((k,x):xs)
+      pick _ [] = undefined
+      pick n ((k,x):ys)
         | n <= k    = x
-        | otherwise = pick (n-k) xs
+        | otherwise = pick (n-k) ys
   in  choose (1, tot) >>= (`pick` xs)
 
 vector :: Arbitrary a => Int -> Gen [a]
