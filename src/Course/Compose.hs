ce1728ea4269a42b10c42dd7de64cc7f4284ff95
Jason Davidson <jdavidson@shujinko.io>
2020-10-09 15:40:23 -0700

    Implement Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Course.Compose where
 
@@ -15,31 +16,27 @@ newtype Compose f g a =
   Compose (f (g a)) deriving (Show, Eq)
 
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  (<$>) :: (a -> b) -> Compose f g a -> Compose f g b
+  ab <$> (Compose fga) = Compose $ (ab <$>) <$> fga
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
--- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  pure :: a -> Compose f g a
+  pure = Compose . pure . pure
 
-instance (Monad f, Monad g) =>
-  Monad (Compose f g) where
+-- Implement the (<*>) function for an Applicative instance for Compose
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  Compose fgab <*> Compose fga = Compose $ lift2 (<*>) fgab fga
+
+instance (Monad f, Monad g) => Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (=<<)#instance (Compose f g)"
+  (=<<) = error "not possible"
 
 -- Note that the inner g is Contravariant but the outer f is
 -- Functor. We would not be able to write an instance if both were
 -- Contravariant; why not?
-instance (Functor f, Contravariant g) =>
-  Contravariant (Compose f g) where
+instance (Functor f, Contravariant g) => Contravariant (Compose f g) where
 -- Implement the (>$<) function for a Contravariant instance for Compose
-  (>$<) =
-    error "todo: Course.Compose (>$<)#instance (Compose f g)"
+  (>$<) :: (b -> a) -> Compose f g a -> Compose f g b
+  f >$< Compose fga = Compose $ (f >$<) <$> fga
