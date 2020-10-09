7aac7b23ea7136f166da56248b4cc9279e0c991f
Jason Davidson <jdavidson@shujinko.io>
2020-10-09 14:40:29 -0700

    Implement Extend typeclass

--- src/Course/Extend.hs
+++ src/Course/Extend.hs
@@ -33,8 +33,7 @@ instance Extend ExactlyOne where
     (ExactlyOne a -> b)
     -> ExactlyOne a
     -> ExactlyOne b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance ExactlyOne"
+  eab <<= ea = ExactlyOne $ eab ea
 
 -- | Implement the @Extend@ instance for @List@.
 --
@@ -51,8 +50,10 @@ instance Extend List where
     (List a -> b)
     -> List a
     -> List b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance List"
+  fs <<= xs = map fs $ tails' xs
+    where
+      tails' Nil = Nil
+      tails' yss@(_:.ys) = yss :. tails' ys
 
 -- | Implement the @Extend@ instance for @Optional@.
 --
@@ -66,8 +67,8 @@ instance Extend Optional where
     (Optional a -> b)
     -> Optional a
     -> Optional b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance Optional"
+  _ <<= Empty = Empty
+  oab <<= oa = Full $ oab oa
 
 -- | Duplicate the functor using extension.
 --
@@ -86,5 +87,4 @@ cojoin ::
   Extend k =>
   k a
   -> k (k a)
-cojoin =
-  error "todo: Course.Extend#cojoin"
+cojoin = (id <<=)
